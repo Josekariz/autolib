dc0bb37bbd1fc9422cdfe462d240233a703dbbfb
Joseph <sejokarizz@gmail.com>
2024-01-31 20:11:38 +0300

    fix footer responsiveness

--- src/app/Footer.tsx
+++ src/app/Footer.tsx
@@ -10,21 +10,21 @@ export default function Footer() {
             <div className="max-w-6xl mx-auto px-4">
                 <div className="flex justify-between items-center flex-col md:flex-row text-lg">
                     {/* Left side: Slogan */}
-                    <div className="mb-4 md:mb-0 flex-1">
-                        <span className="text-black text-md md:text-xl lg:text-2xl">The ultimate car review listing site</span>
+                    <div className="mb-4 md:mb-0 flex-1 text-center md:text-left">
+                        <span className="text-black text-xs sm:text-sm md:text-lg lg:text-xl xl:text-2xl">The ultimate car review listing site</span>
                     </div>
 
                     {/* Right side: Contact Information */}
-                    <div className="flex flex-col text-white text-sm space-y-2">
-                        <Link href="https://www.instagram.com/sejoqariz/" className="hover:underline ">
-                            <FaInstagram className="inline"/> Instagram
+                    <div className="flex flex-col items-center md:items-end text-white text-xs sm:text-sm md:text-base space-y-2">
+                        <Link href="https://www.instagram.com/sejoqariz/" className="hover:underline">
+                            <span><FaInstagram className="inline text-lg md:text-xl"/> Instagram</span>
                         </Link>
-                        <a href="mailto:dummyemail@example.com" className="hover:underline">
-                            <FaEnvelope className="inline"/> dummyemail@example.com
-                        </a>
-                        <a href="tel:1234567890" className="hover:underline">
-                            <FaPhone className="inline"/> (123) 456-7890
-                        </a>
+                        <Link href="mailto:dummyemail@example.com" className="hover:underline">
+                            <span><FaEnvelope className="inline text-lg md:text-xl"/> dummyemail@example.com</span>
+                        </Link>
+                        <Link href="tel:1234567890" className="hover:underline">
+                            <span><FaPhone className="inline text-lg md:text-xl"/> (123) 456-7890</span>
+                        </Link>
                     </div>
                 </div>
 
@@ -32,7 +32,7 @@ export default function Footer() {
                 <hr className="my-4 border-t border-white" />
 
                 {/* Bottom Center: Copyright */}
-                <div className="text-center text-lg">
+                <div className="text-center text-xs sm:text-sm md:text-base lg:text-lg">
                     <span className="text-white">&copy; {currentYear}</span>
                 </div>
             </div>
